bc549404047d380910a1801d1046b33dbcdedd04
jaburtof <43141692+jaburtof@users.noreply.github.com>
2026-07-22 21:33:07 -0600

    Add files via upload

---
+++ src/main/java/cr/ac/cenfotec/pixelforge/PixelforgeMathengineApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.cenfotec.pixelforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelforgeMathengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelforgeMathengineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cr/ac/cenfotec/pixelforge/PixelforgeMathengineApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.cenfotec.pixelforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelforgeMathengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
